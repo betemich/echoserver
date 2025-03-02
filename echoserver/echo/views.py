23ca24dc174d46a04506a480e55e5f4be98aa9e4
betemich <betekhtinms@ya.ru>
2025-03-02 11:05:03 +0500

    First commit

---
+++ echoserver/echo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import Book
+from .forms import BookForm
+
+def book_list(request):
+    books = Book.objects.all().order_by('title')
+    paginator = Paginator(books, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'books/book_list.html', {'page_obj': page_obj})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_create.html', {'form': form})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/book_update.html', {'form': form})
+    
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/book_delete.html', {'book': book})
